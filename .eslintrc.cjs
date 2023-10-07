b4a3d92462218a26546f1b6cf50925151b044c80
M Haidar Hanif <git@mhaidarhanif.com>
2023-10-07 21:32:02 +0700

    chore: setup eslint prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,9 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  rules: {
+    "react/self-closing-comp": ["warn", { component: true, html: true }],
+    "@typescript-eslint/no-unused-vars": ["warn"],
+    "require-await": ["warn"],
+  },
 };
