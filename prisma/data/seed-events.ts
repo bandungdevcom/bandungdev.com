188b731ff24ea14742007dc37fe1bcf34b06f03d
M Haidar Hanif <git@mhaidarhanif.com>
2023-10-11 08:23:13 +0700

    feat: setup prisma and planetscale

---
+++ prisma/data/seed-events.ts
@@ -0,0 +1,7 @@
+export const dataSeedEvents = [
+  { title: "Official Setup/Launch" },
+  { title: "Community and Productivity" },
+  { title: "Coffee and Tea" },
+  { title: "Learn JavaScript" },
+  { title: "Coffee and Tea" },
+];
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,19 +1,29 @@
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "~/db.server";
 
-const prisma = new PrismaClient();
+import { dataSeedEvents } from "./data";
 
 async function main() {
-  // Seed your database here
+  console.info("NODE_ENV", process.env.NODE_ENV);
+
+  await seedEvents();
+}
+
+async function seedEvents() {
+  const createdEvents = await prisma.event.createMany({
+    data: dataSeedEvents,
+  });
+
+  console.info(`🟢 Created Events: ${createdEvents.count} events`);
 }
 
 main()
   .then(async () => {
-    console.log('Seeding complete');
+    console.log("Seeding complete");
     await prisma.$disconnect();
   })
-  .catch((e) => {
+  .catch(e => {
     console.error(e);
-    console.log('Seeding failed');
+    console.log("Seeding failed");
     prisma.$disconnect();
     process.exit(1);
   });
