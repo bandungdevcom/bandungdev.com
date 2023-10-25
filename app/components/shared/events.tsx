59a3888f5b3a817a4688c553e4210a5ba7ad9782
M Haidar Hanif <git@mhaidarhanif.com>
2023-10-26 04:07:12 +0700

    feat: upcoming and past events

--- app/components/shared/events.tsx
+++ app/components/shared/events.tsx
@@ -1,39 +1,36 @@
-import { formatDateTimeRelative } from "~/utils/datetime";
-import { Card } from "~/components/ui/card";
+import { type Event } from '@prisma/client'
 
-export function EventsList({
-  events,
-}: {
-  events: {
-    title: string;
-    id: string;
-    description: string | null;
-    datetime: Date | null;
-  }[];
-}) {
-  return (
-    <ul className="space-y-4">
-      {events.map(event => (
-        <li key={event.id}>
-          <Card className="flex gap-4 p-4">
-            <div>
-              <img
-                className="rounded-lg"
-                src="https://picsum.photos/300/200"
-                alt={event.title}
-              />
-            </div>
+import { formatDateTimeRelative } from '~/utils/datetime'
+import { Card } from '~/components/ui/card'
 
-            <div className="space-y-4">
-              <h3>{event.title}</h3>
-              <p className="text-muted-foreground">{event.description}</p>
-              <p>
-                <time>{formatDateTimeRelative(event.datetime)}</time>
-              </p>
-            </div>
-          </Card>
-        </li>
-      ))}
-    </ul>
-  );
+export function EventsList({ events }: { events: Event[] }) {
+	if (events.length <= 0) {
+		return <p>No events or still in planning.</p>
+	}
+
+	return (
+		<ul className="space-y-4">
+			{events.map(event => (
+				<li key={event.id}>
+					<Card className="flex gap-4 p-4">
+						<div>
+							<img
+								className="rounded-lg"
+								src="https://picsum.photos/300/200"
+								alt={event.title}
+							/>
+						</div>
+
+						<div className="space-y-4">
+							<h3>{event.title}</h3>
+							<p className="text-muted-foreground">{event.description}</p>
+							<p>
+								<time>{formatDateTimeRelative(event.datetime)}</time>
+							</p>
+						</div>
+					</Card>
+				</li>
+			))}
+		</ul>
+	)
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,68 +1,84 @@
-import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node'
+import { json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
 
-import { prisma } from "~/libs/db.server";
-import { EventsList } from "~/components/shared/events";
-import { ButtonLink } from "~/components/ui/button-link";
+import { prisma } from '~/libs/db.server'
+import { EventsList } from '~/components/shared/events'
+import { ButtonLink } from '~/components/ui/button-link'
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "BandungDev" },
-    {
-      name: "description",
-      content: "Komunitas developer Bandung yang terkurasi.",
-    },
-  ];
-};
+	return [
+		{ title: 'BandungDev' },
+		{
+			name: 'description',
+			content: 'Komunitas developer Bandung yang terkurasi.',
+		},
+	]
+}
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const events = await prisma.event.findMany({
-    where: { isFeatured: true },
-  });
+	const [upcomingEvents, pastEvents] = await prisma.$transaction([
+		prisma.event.findMany({
+			where: {
+				datetime: { gte: new Date() },
+				isFeatured: true,
+			},
+		}),
+		prisma.event.findMany({
+			where: {
+				datetime: { lte: new Date() },
+				isFeatured: true,
+			},
+		}),
+	])
 
-  return json({ events });
-};
+	return json({ upcomingEvents, pastEvents })
+}
 
 export default function Index() {
-  const { events } = useLoaderData<typeof loader>();
+	const { upcomingEvents, pastEvents } = useLoaderData<typeof loader>()
 
-  return (
-    <div>
-      <section className="section-auto sm:py-16 md:py-28">
-        <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-          <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-slate-600 ring-1 ring-slate-900/10 transition-all hover:ring-slate-900/20">
-            BandungDev website is a work in progress
-          </div>
-        </div>
+	return (
+		<div>
+			<section className="section-auto sm:py-16 md:py-28">
+				<div className="hidden sm:mb-8 sm:flex sm:justify-center">
+					<div className="relative rounded-full px-3 py-1 text-sm leading-6 text-slate-600 ring-1 ring-slate-900/10 transition-all hover:ring-slate-900/20">
+						BandungDev website is a work in progress
+					</div>
+				</div>
 
-        <div className="space-y-6 text-center">
-          <h1 className="text-4xl font-bold tracking-tight text-blue-950 sm:text-7xl">
-            BandungDev
-          </h1>
+				<div className="space-y-6 text-center">
+					<h1 className="text-4xl font-bold tracking-tight text-blue-950 sm:text-7xl">
+						BandungDev
+					</h1>
 
-          <p className="text-base text-secondary-foreground sm:text-lg">
-            The curated software developer and engineering community in Bandung,
-            Indonesia. BandungDev is collaborating with various other tech
-            communities. We aim to improve the educational activities.{" "}
-            <code>#HelloHelloBandungDev</code>
-          </p>
+					<p className="text-base text-secondary-foreground sm:text-lg">
+						The curated software developer and engineering community in Bandung,
+						Indonesia. BandungDev is collaborating with various other tech
+						communities. We aim to improve the educational activities.{' '}
+						<code>#HelloHelloBandungDev</code>
+					</p>
 
-          <div className="flex-center flex-wrap gap-4">
-            <ButtonLink size="lg" to="/signup">
-              Sign Up
-            </ButtonLink>
-            <ButtonLink size="lg" to="/events" variant="secondary">
-              Explore Events
-            </ButtonLink>
-          </div>
-        </div>
-      </section>
+					<div className="flex-center flex-wrap gap-4">
+						<ButtonLink size="lg" to="/signup">
+							Sign Up
+						</ButtonLink>
+						<ButtonLink size="lg" to="/events" variant="secondary">
+							Explore Events
+						</ButtonLink>
+					</div>
+				</div>
+			</section>
 
-      <section className="section-auto space-y-8">
-        <h2>Upcoming Events</h2>
-        <EventsList events={events as any} />
-      </section>
-    </div>
-  );
+			<section className="section-auto space-y-8">
+				<h2>Upcoming Events</h2>
+				<EventsList events={upcomingEvents as any} />
+			</section>
+
+			<section className="section-auto space-y-8">
+				<h2>Past Events</h2>
+				<EventsList events={pastEvents as any} />
+			</section>
+		</div>
+	)
 }
