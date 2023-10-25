4f05d71ed4e1701c60d5b0e0229519505f282d2c
M Haidar Hanif <git@mhaidarhanif.com>
2023-10-26 03:23:13 +0700

    feat: social link isEnabled

--- app/components/shared/social-links.tsx
+++ app/components/shared/social-links.tsx
@@ -1,21 +1,23 @@
-import { Icon } from "@iconify/react";
+import { Icon } from '@iconify/react'
 
-import { configSocialLinks } from "~/configs/social-links";
-import { Anchor } from "~/components/ui/anchor";
+import { configSocialLinks } from '~/configs/social-links'
+import { Anchor } from '~/components/ui/anchor'
 
 export function SocialLinks() {
-  return (
-    <ul className="flex flex-wrap items-center gap-4">
-      {configSocialLinks.map(socialLink => (
-        <li key={socialLink.text}>
-          <Anchor
-            href={socialLink.href}
-            className="block rounded-md bg-blue-950 p-2 text-2xl text-blue-300 transition-opacity hover:opacity-70"
-          >
-            <Icon icon={socialLink.icon} />
-          </Anchor>
-        </li>
-      ))}
-    </ul>
-  );
+	return (
+		<ul className="flex flex-wrap items-center gap-4">
+			{configSocialLinks
+				.filter(item => item.isEnabled)
+				.map(item => (
+					<li key={item.text}>
+						<Anchor
+							href={item.href}
+							className="block rounded-md bg-blue-950 p-2 text-2xl text-blue-300 transition-opacity hover:opacity-70"
+						>
+							<Icon icon={item.icon} />
+						</Anchor>
+					</li>
+				))}
+		</ul>
+	)
 }
