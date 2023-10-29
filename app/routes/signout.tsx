85004d7bf8e53e2b40846eff332b749ab4facb5b
aguswesly <agustchannel@gmail.com>
2023-10-29 16:07:38 +0700

    fix : remove async keyword to resolve warning

--- app/routes/signout.tsx
+++ app/routes/signout.tsx
@@ -4,15 +4,13 @@ import {
 	type LoaderFunctionArgs,
 } from "@remix-run/node"
 
-// eslint-disable-next-line require-await
-export async function action ({ request }: ActionFunctionArgs) {
+export function action({ request }: ActionFunctionArgs) {
 	// Clear credential
 
 	return redirect("/signin")
 }
 
-// eslint-disable-next-line require-await
-export async function loader({ request }: LoaderFunctionArgs) {
+export function loader({ request }: LoaderFunctionArgs) {
 	// Clear credential
 
 	return redirect("/signin")
