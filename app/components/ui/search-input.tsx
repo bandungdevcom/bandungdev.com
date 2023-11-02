2d18bdce74e96734381325774e54a1417e0d4bc3
aguswesly <agustchannel@gmail.com>
2023-11-02 17:35:45 +0700

    feat : search events functionality

--- app/components/ui/search-input.tsx
+++ app/components/ui/search-input.tsx
@@ -4,7 +4,7 @@ import { cn } from "~/utils/cn"
 
 export interface SearchInputProps
 	extends React.InputHTMLAttributes<HTMLElement> {
-	setValue: React.ChangeEventHandler<HTMLInputElement>
+	setValue?: React.ChangeEventHandler<HTMLInputElement>
 }
 
 export const SearchInput = React.forwardRef<HTMLInputElement, SearchInputProps>(
@@ -13,21 +13,19 @@ export const SearchInput = React.forwardRef<HTMLInputElement, SearchInputProps>(
 		ref,
 	) => {
 		return (
-			<label htmlFor={name} className="block">
-				<input
-					ref={ref}
-					type={type}
-					name={name}
-					className={cn(
-						"w-full rounded-full bg-secondary-foreground/10 px-6 py-4 text-xs font-medium outline-none backdrop-blur-sm placeholder:text-secondary-foreground/60 md:text-base",
-						className,
-					)}
-					placeholder={placeholder}
-					value={value}
-					onChange={setValue}
-					{...props}
-				/>
-			</label>
+			<input
+				ref={ref}
+				type={type}
+				name={name}
+				className={cn(
+					"w-full rounded-full bg-secondary-foreground/10 px-6 py-4 text-xs font-medium outline-none backdrop-blur-sm placeholder:text-secondary-foreground/60 md:text-base",
+					className,
+				)}
+				placeholder={placeholder}
+				value={value}
+				onChange={setValue}
+				{...props}
+			/>
 		)
 	},
 )
--- app/routes/events.tsx
+++ app/routes/events.tsx
@@ -1,10 +1,16 @@
-import { useState } from "react"
 import {
 	json,
 	type LoaderFunctionArgs,
 	type MetaFunction,
 } from "@remix-run/node"
-import { useLoaderData } from "@remix-run/react"
+import {
+	Form,
+	useLoaderData,
+	useNavigation,
+	useSearchParams,
+} from "@remix-run/react"
+import type { Prisma } from "@prisma/client"
+import type { DefaultArgs } from "@prisma/client/runtime/library"
 
 import { prisma } from "~/libs/db.server"
 import { EventsList } from "~/components/shared/events"
@@ -16,13 +22,40 @@ export const meta: MetaFunction = () => [
 ]
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-	const events = await prisma.event.findMany()
+	const routeUrl = new URL(request.url)
+	const keyword = routeUrl.searchParams.get("keyword")
+
+	let filterArgs: Prisma.EventFindManyArgs<DefaultArgs> = {}
+
+	if (keyword) {
+		filterArgs = {
+			where: {
+				OR: [
+					{
+						title: {
+							contains: keyword,
+						},
+					},
+					{
+						description: {
+							contains: keyword,
+						},
+					},
+				],
+			},
+		}
+	}
+
+	const events = await prisma.event.findMany(filterArgs)
 	return json({ events })
 }
 
 export default function EventsRoute() {
 	const { events } = useLoaderData<typeof loader>()
-	const [searchValue, setSearchValue] = useState("")
+	const [searchParams] = useSearchParams()
+	const { state } = useNavigation()
+
+	const isSearching = state === "loading"
 
 	return (
 		<div>
@@ -31,17 +64,36 @@ export default function EventsRoute() {
 			<section className="section-auto relative">
 				<header className="space-y-4">
 					<h1 className="mb-10 text-center">Event</h1>
-					<SearchInput
-						className="mx-auto block max-w-md"
-						placeholder="Cari event, contoh: Javascript"
-						value={searchValue}
-						setValue={evt => setSearchValue(evt.target.value)}
-					/>
+					<Form className="relative w-full">
+						<SearchInput
+							className="block w-full pr-12"
+							placeholder="Cari event, contoh: Javascript"
+							name="keyword"
+							defaultValue={searchParams.get("keyword") || ""}
+						/>
+
+						<button className="absolute right-5 top-3 md:top-4" type="submit">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								strokeWidth={1.5}
+								stroke="currentColor"
+								className="h-6 w-6"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+								/>
+							</svg>
+						</button>
+					</Form>
 				</header>
 			</section>
 
 			<section className="section-content relative space-y-4">
-				<EventsList events={events} />
+				{isSearching ? <p>Searching...</p> : <EventsList events={events} />}
 			</section>
 		</div>
 	)
