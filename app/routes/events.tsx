51e6b2b758ab48ddd28bc380655f033b4f6e6747
aguswesly <agustchannel@gmail.com>
2023-11-03 08:39:46 +0700

    fix : using icon from iconify

--- app/routes/events.tsx
+++ app/routes/events.tsx
@@ -9,6 +9,7 @@ import {
 	useNavigation,
 	useSearchParams,
 } from "@remix-run/react"
+import { Icon } from "@iconify/react"
 import type { Prisma } from "@prisma/client"
 import type { DefaultArgs } from "@prisma/client/runtime/library"
 
@@ -72,21 +73,8 @@ export default function EventsRoute() {
 							defaultValue={searchParams.get("keyword") || ""}
 						/>
 
-						<button className="absolute right-5 top-3 md:top-4" type="submit">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								strokeWidth={1.5}
-								stroke="currentColor"
-								className="h-6 w-6"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-								/>
-							</svg>
+						<button className="absolute right-5 top-0 h-full" type="submit">
+							<Icon icon="octicon:search-24" width={20} height={20} />
 						</button>
 					</Form>
 				</header>
