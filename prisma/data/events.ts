8d2a4d9c8f27e0e4cdcb3b0c1ece2b26a1e87f6a
M Haidar Hanif <git@mhaidarhanif.com>
2023-12-11 04:48:44 +0700

    feat: model category for event and location

---
+++ prisma/data/events.ts
@@ -0,0 +1,29 @@
+export const dataEvents = [
+  {
+    title: "Official Setup/Launch",
+    description: "The description of the event.",
+    date: new Date("2023-10-22"),
+    timeStart: new Date("13:00"),
+    timeEnd: new Date("18:00"),
+    statusSymbol: "PUBLISHED",
+    categorySymbol: "MEETING",
+  },
+  {
+    title: "Casual Meetup in November",
+    description: "The description of the event.",
+    date: new Date("2023-11-11"),
+    timeStart: new Date("13:00"),
+    timeEnd: new Date("16:00"),
+    statusSymbol: "PUBLISHED",
+    categorySymbol: "MEETUP",
+  },
+  {
+    title: "Casual Meetup in December",
+    description: "The description of the event.",
+    date: new Date("2023-12-30"),
+    timeStart: new Date("14:00"),
+    timeEnd: new Date("17:00"),
+    statusSymbol: "UNLISTED",
+    categorySymbol: "MEETUP",
+  },
+]
