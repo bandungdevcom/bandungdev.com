bfecae5e7c3f17f3eef572bb40b5eb103046e8c1
fikrialwan <fikriar93@gmail.com>
2023-12-17 16:47:31 +0700

    feat: adjustment gradient color

--- app/components/shared/background-gradient-full-height.tsx
+++ app/components/shared/background-gradient-full-height.tsx
@@ -3,12 +3,12 @@ import { cn } from "~/utils/cn"
 export function BackgroundGradientFullHeight() {
   return (
     <div
-      className={cn(
-        "fixed top-0 left-0 right-0 -z-10 block h-screen w-screen",
-        "bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]",
-        "from-slate-400/50 via-slate-200/25 to-slate-100/20",
-        "dark:from-slate-800 dark:via-slate-900/25 dark:to-slate-950/20",
-      )}
+    className={cn(
+      "absolute left-0 right-0 -z-10 block h-screen w-full",
+      "bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]",
+      "from-slate-400/50 via-slate-100/20 to-white",
+      "dark:from-slate-800 dark:via-slate-900/25 dark:to-slate-950",
+    )}
     />
   )
 }
