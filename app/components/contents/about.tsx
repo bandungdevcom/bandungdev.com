fcdeaa6546ec7be665874d1f7d1983ac0d5b5664
fikrialwan <fikriar93@gmail.com>
2023-12-18 22:59:17 +0700

    feat: using prose-config on about

--- app/components/contents/about.tsx
+++ app/components/contents/about.tsx
@@ -1,6 +1,5 @@
 import { Theme, useTheme } from "remix-themes"
 
-import { Badge } from "~/components/ui/badge"
 import { cn } from "~/utils/cn"
 
 export function ContentAbout() {
@@ -12,7 +11,7 @@ export function ContentAbout() {
 
   return (
     <div className="space-y-10">
-      <header>
+      <header className="flex justify-center">
         <div
           className={cn(
             "inline-flex select-none flex-wrap items-center justify-center gap-2 sm:gap-4",
@@ -37,22 +36,22 @@ export function ContentAbout() {
         </div>
       </header>
 
-      <div className="space-y-2">
-        <p className="sm:text-lg">
+      <div className="prose-config space-y-2">
+        <p>
           BandungDev is a tech community for software developers, programmers,
           engineers in Bandung area and around it.
         </p>
-        <p className="sm:text-lg">
+        <p>
           The tech stack focus is quite general ranging from web, mobile, even
           desktop. Can either using any coding languages like HTML and CSS, also
           programming languages like JavaScript, TypeScript, Node.js, Java,
           Golang, Kotlin, Rust, and more.
         </p>
-        <p className="sm:text-lg">
+        <p>
           Therefore each event or discussion would be either combining different
           topics or focusing on just one tech in particular.
         </p>
-        <p className="sm:text-lg">
+        <p>
           Based on this idea, BandungDev is being initiated and collaborating
           with various other tech communities such as JVM Indonesia, Programmer
           Zaman Now (PZN), Web Programming UNPAS (WPU), GDG Bandung (Google
@@ -60,28 +59,24 @@ export function ContentAbout() {
           Bandung (ADB), BandungJS, ReactJS Indonesia, Bearmentor, Binary
           Nusantara, and much more.
         </p>
-        <p className="sm:text-lg">
+        <p>
           Our committee members are: M Haidar Hanif, Hendi Santika, Kresna
           Galuh, Eko Kurniawan Khannedy, Sandhika Galih, Dani Sofyan, Erico
           Darmawan Handoyo, Setia Budi, Deni Rohimat, Esa Firman, Hadian Rahmat,
           Didiet Pambudiono, Faqihza Mukhlis (Pukis), Muhammad Mustadi (Odi),
           and much more.
         </p>
-        <p className="sm:text-lg">
+        <p>
           BandungDev primary public community group is on Telegram that just
           opened on 22 October 2023, with more than 1450 members (in late
           October 2023).
         </p>
-        <p className="sm:text-lg">
+        <p>
           Our vision is to collaborate with many other tech and software
           communities in Bandung and around it. Also to inspire other cities to
           build and run their own software developer community. Even BandungDev
           was inspired by SurabayaDev which already run since a few years ago.
         </p>
-
-        <Badge variant="outline" className="font-mono text-base tracking-wider">
-          #HelloHelloBandungDev
-        </Badge>
       </div>
     </div>
   )
