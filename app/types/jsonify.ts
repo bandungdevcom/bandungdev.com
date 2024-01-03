077c0461adfe695710cc04875a186cb052c7a4db
aguswesly <agustchannel@gmail.com>
2024-01-03 12:31:31 +0700

    fix : import path

--- app/types/jsonify.ts
+++ app/types/jsonify.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/consistent-type-specifier-style
-import type { Jsonify } from "node_modules/.pnpm/@remix-run+server-runtime@2.4.1_typescript@5.3.3/node_modules/@remix-run/server-runtime/dist/jsonify.d.ts"
+import type { Jsonify } from "@remix-run/server-runtime/dist/jsonify.d.ts"
 
 export type JsonifyValues<T> = {
   [K in keyof T]: Jsonify<T[K]>
