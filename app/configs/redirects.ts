2c1fb45e5cba6bd08c54085ed473e70d183333ca
M Haidar Hanif <git@mhaidarhanif.com>
2024-01-10 12:17:06 +0700

    docs: about content

--- app/configs/redirects.ts
+++ app/configs/redirects.ts
@@ -1,6 +1,7 @@
 import { type ConfigRedirect } from "~/utils/redirect-route.server"
 
 export const configRedirects: ConfigRedirect[] = [
+  { path: "/register", url: "https://a.bandungdev.com/register" },
   { path: "/me", to: "/user" },
   { path: "/account", to: "/user/account" },
   { path: "/dev", url: "https://github.com/bandungdevcom/bandungdev.com" },
